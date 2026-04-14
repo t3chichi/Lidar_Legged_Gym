7747e2e286026de9294ce630124ace9cfaee00b3
t3chichi <zihewang0129@outlook.com>
2026-04-15 01:15:38 +0800

    reward_fix

--- legged_gym/legged_gym/envs/go2/lidar_pd_risknet/go2_pd_pretrain_config.py
+++ legged_gym/legged_gym/envs/go2/lidar_pd_risknet/go2_pd_pretrain_config.py
@@ -115,6 +115,7 @@ class Go2LidarPDRiskNetCfg(Go2RoughCfg):
             orientation = -5.0
             torques = -0.000025
             feet_air_time = 1.5
+            lin_vel_z = -3.0
             feet_contact_forces = -0.01
             gait_scheduler = -3
 
