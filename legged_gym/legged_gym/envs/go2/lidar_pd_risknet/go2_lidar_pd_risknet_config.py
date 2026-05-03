aff4908565f0ff64d8e10eae9a06801483993ee7
t3chichi <zihewang0129@outlook.com>
2026-05-03 15:06:27 +0800

    关闭课程学习

--- legged_gym/legged_gym/envs/go2/lidar_pd_risknet/go2_lidar_pd_risknet_config.py
+++ legged_gym/legged_gym/envs/go2/lidar_pd_risknet/go2_lidar_pd_risknet_config.py
@@ -106,7 +106,7 @@ class Go2LidarPDRiskNetCfg(Go2RoughCfg):
     class commands(Go2RoughCfg.commands):
         heading_command = False
         resampling_time = 2.
-        curriculum = True
+        curriculum = False
         class ranges(Go2RoughCfg.commands.ranges):
             lin_vel_x = [0.1, 0.9]  # min max [m/s]
             lin_vel_y = [-0.2, 0.2]  # min max [m/s]
