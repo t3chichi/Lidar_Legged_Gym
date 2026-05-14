3365890f103fcd9ce0be1f08b57197ea1fe10630
t3chichi <zihewang0129@outlook.com>
2026-05-14 19:27:17 +0800

    增加垂直抖动惩罚

--- legged_gym/legged_gym/envs/go2/lidar_pd_risknet/go2_lidar_pd_risknet_config.py
+++ legged_gym/legged_gym/envs/go2/lidar_pd_risknet/go2_lidar_pd_risknet_config.py
@@ -162,6 +162,9 @@ class Go2LidarPDRiskNetCfg(Go2RoughCfg):
             ang_vel_xy = -5.0e-2
             orientation = -1.0
 
+            #override
+            lin_vel_z = -1.0e-3
+
             y_progress = 1.0  # 世界坐标系 Y 进度奖励，鼓励沿走廊持续前进
             goal = 5.0  # 通道终点到达奖励（任务特有，论文无通道场景）
             # ang_vel_yaw_penalty = -2.0e-2  # 惩罚过大偏航角速度，鼓励稳定朝向
