3b03e4968a92b03ac1946700e5135f5cdd56f120
t3chichi <zihewang0129@outlook.com>
2026-05-17 20:57:10 +0800

    fix

--- legged_gym/legged_gym/envs/go2/lidar_pd_risknet/go2_lidar_pd_risknet_config.py
+++ legged_gym/legged_gym/envs/go2/lidar_pd_risknet/go2_lidar_pd_risknet_config.py
@@ -157,10 +157,10 @@ class Go2LidarPDRiskNetCfg(Go2RoughCfg):
             tracking_lin_vel = 5.0e-1   
             tracking_ang_vel = 0.0   
             feet_air_time = 1.0      
-            base_height = -1.0   
+            base_height = -5.0   
             gait_2_step = -5.0e-1    
             ang_vel_xy = -5.0e-2
-            orientation = -1.0
+            orientation = -5.0
 
             #override
             # lin_vel_z = -1.0e-3
