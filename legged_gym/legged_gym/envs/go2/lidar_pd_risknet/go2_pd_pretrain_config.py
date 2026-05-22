de04a0bf1c1beea23ecb33c2be712dcbb9b1a3de
t3chichi <zihewang0129@outlook.com>
2026-05-22 13:32:16 +0800

    fix

--- legged_gym/legged_gym/envs/go2/lidar_pd_risknet/go2_pd_pretrain_config.py
+++ legged_gym/legged_gym/envs/go2/lidar_pd_risknet/go2_pd_pretrain_config.py
@@ -146,6 +146,7 @@ class Go2LidarPDRiskNetCfg(Go2RoughCfg):
             torques = -0.000025  # 覆盖的力矩惩罚权重：更严格地限制关节力矩
             feet_air_time = 1.0  # 覆盖的足端离地时间权重：保持/强调期望步态空中时间
             ang_vel_xy = -0.1
+            base_height = -2.0
             # feet_contact_forces = -0.01
             # gait_scheduler = -3
 
