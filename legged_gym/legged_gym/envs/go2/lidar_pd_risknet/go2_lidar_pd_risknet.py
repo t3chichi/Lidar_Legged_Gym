32fbd2150321d763b72aa8a791a55d874d6792d6
t3chichi <zihewang0129@outlook.com>
2026-05-25 16:08:49 +0800

    增加点云密度

--- legged_gym/legged_gym/envs/go2/lidar_pd_risknet/go2_lidar_pd_risknet.py
+++ legged_gym/legged_gym/envs/go2/lidar_pd_risknet/go2_lidar_pd_risknet.py
@@ -664,7 +664,7 @@ class Go2LidarPDRiskNet(Go2):
 
         # Downsample to ~256 points total for performance.
         num_pts = pts_base.shape[0]
-        max_draw = min(256, num_pts)
+        max_draw = min(864, num_pts)
         step = max(1, num_pts // max_draw)
 
         # Draw proximal points (yellow)
