7cbdfdb97d0216886aa826318f88f8afb5a24a79
t3chichi <zihewang0129@outlook.com>
2026-06-06 13:46:47 +0800

    feat: 降低指令奖励, 提高曲率惩罚

--- legged_gym/legged_gym/envs/go2/lidar_pd_risknet/go2_lidar_pd_risknet_config.py
+++ legged_gym/legged_gym/envs/go2/lidar_pd_risknet/go2_lidar_pd_risknet_config.py
@@ -171,8 +171,8 @@ class Go2LidarPDRiskNetCfg(Go2RoughCfg):
             action_rate = -5.0e-3  # 一阶动作平滑惩罚：限制相邻时刻动作变化
             action_rate2 = -5.0e-3  # 二阶动作平滑惩罚：限制动作”抖动/顿挫”
 
-            tracking_lin_vel = 1.0   
-            tracking_ang_vel = 0.5  
+            tracking_lin_vel = 0.5   
+            tracking_ang_vel = 0.1  
             feet_air_time = 1.0      
             gait_2_step = -5.0e-1    
             ang_vel_xy = -5.0e-2
@@ -185,7 +185,7 @@ class Go2LidarPDRiskNetCfg(Go2RoughCfg):
 
             goal = 10.0  # 通道终点到达奖励（任务特有，论文无通道场景）
             # ang_vel_yaw_penalty = -2.0e-2  # 惩罚过大偏航角速度，鼓励稳定朝向
-            curvature = -0.05  # 曲率惩罚：抑制 ω_z²/(v_xy²+σ²)，防止原地转圈
+            curvature = -0.1  # 曲率惩罚：抑制 ω_z²/(v_xy²+σ²)，防止原地转圈
 
 
     class normalization(Go2RoughCfg.normalization):
